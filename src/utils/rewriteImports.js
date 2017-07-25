65287f5b90314a26ff21a566aba3b143f916da87
Nicholas Rawlings <nicholas.rawlings@pointsource.com>
2017-07-25 11:38:39 -0400

    Exclude @imports that begin with '~'
    The tilde is a special syntax used by WebPack to denote a path relative to the project root.  We don’t want to rewrite those.

--- src/utils/rewriteImports.js
+++ src/utils/rewriteImports.js
@@ -2,7 +2,7 @@ import path from 'path';
 
 import logger from './logger';
 
-const importRegexp = /@import\s+(?:'([^']+)'|"([^"]+)"|([^\s;]+))/g;
+const importRegexp = /@import\s+(?:'([^'~][^']*)'|"([^"~][^"]*)"|([^\s;~][^\s;]*))\s*(?:;|$)/g;
 
 export default (error, file, contents, moduleContext, callback) => {
   if (error) {
@@ -20,6 +20,11 @@ export default (error, file, contents, moduleContext, callback) => {
     const newImportPath = path.relative(moduleContext, absoluteImportPath);
 
     logger.debug(`Resources: @import of ${oldImportPath} changed to ${newImportPath}`);
+
+	console.log();
+	console.log(entire);
+    console.log(`Resources: context is ${moduleContext}`);
+    console.log(`Resources: @import of ${oldImportPath} changed to ${newImportPath}`);
 
     const lastCharacter = entire[entire.length - 1];
     const quote = lastCharacter === "'" || lastCharacter === '"' ? lastCharacter : '';
